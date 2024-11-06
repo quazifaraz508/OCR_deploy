4d42764f1758d4a369917b8dfefb7855dc6bb6e5
quazifaraz508 <141524293+quazifaraz508@users.noreply.github.com>
2024-11-06 22:20:25 +0530

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,2 @@
-
-
-# Update packages and install Tesseract
+#!/usr/bin/env bash
 apt-get update && apt-get install -y tesseract-ocr
-
-# Run any additional build steps if needed
-# e.g., Django migrations or static file collection
-python manage.py migrate
-python manage.py collectstatic --noinput
