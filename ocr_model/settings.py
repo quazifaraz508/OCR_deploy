5060b53d0a54d27b17e2fcc24e742c696d992501
quazifaraz508 <141524293+quazifaraz508@users.noreply.github.com>
2024-11-12 22:32:48 +0530

    settings.py

--- ocr_model/settings.py
+++ ocr_model/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['docusight.onrender.com', 'docusight.vercel.com' ]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
